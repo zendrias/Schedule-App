e47f1cdbbc258c428e599a6b662529229bf0a174
zendrias <zenas@live.com>
2022-03-26 20:22:26 -0400

    search bar

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,5 +1,24 @@
+import subjectList from '../../public/data/subjectlist.json';
+import  {useState} from 'react'
+import Image from 'next/image';
+import Logo from '/images/logo.jpg'
+
 const Header = () => {
-  return <header className="fixed inset-0 bg-blue-400 h-20">Header</header>;
+  const [searchData, setSearchData] = useState({
+    searchBar: ''
+  })
+
+  function handleChange(e) {
+    setSearchData({...searchData, [e.target.name] : e.target.value})
+  }
+  return (
+    <header className="fixed inset-0 bg-blue-400 h-20 flex justify-center items-center" >
+      <Image src={Logo} width="95" alt='logo' height="95"/ >
+      <p className='font-bold font-sans mx-5 text-2xl'>Search Courses</p>
+      {/* <i className='absolute'>🔎</i> */}
+      <input name='searchBar' value={searchData.searchBar} onChange={handleChange} type="text"className="bg-green-100 h-16  w-1/2 font-bold text-2xl rounded-xl border-4 border-orange-300 p-2" />
+    </header>
+  )
 };
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head';
 import Calendar from '../components/Calendar/Calendar';
 import Header from '../components/Header/Header';
 import Sidebar from '../components/Sidebar/Sidebar';
-
 export default function Home() {
+
+
   return (
     <>
       <Head>
